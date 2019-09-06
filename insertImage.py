88d5fbb941be2ed0d7807299531cefd8bc921ddd
小久保 剛基 <yoshiki.kokubo@ted.ntsa.kubota.co.jp>
2019-09-06 14:31:02 +0900

    コメント日本語化、引継ぎ用修正

--- insertImage.py
+++ insertImage.py
@@ -4,7 +4,6 @@ import PyPDF2 as pypdf
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 import os
-from tkinter import messagebox
 import configparser
 
 # 設定ファイルのパスから設定情報を取得
@@ -12,35 +11,33 @@ CONF_FILEPATH = "./config/myconf.conf"
 config = configparser.ConfigParser()
 config.read(CONF_FILEPATH, 'UTF-8')
 
-# Proxy関連
 config_val = config['values']
 x_pos = config_val['x_pos']
 y_pos = config_val['y_pos']
 img_name = config_val['img_name']
 
 
+# pdfマージシステム利用
 def gen_pdf(original_file_list):
     for original_file in original_file_list:
-        # there are 66 slides (1.jpg, 2.jpg, 3.jpg...)
         path = 'image/'+img_name
         pdf = pypdf.PdfFileWriter()
 
-        # Using ReportLab Canvas to insert image into PDF
+        # reportlabのキャンバスを利用する
         imgTemp = BytesIO()
         imgDoc = canvas.Canvas(imgTemp, pagesize=A4)
-        # Draw image on Canvas and save PDF in buffer
-        # (self, image, x,y,width,height,mask=None)
-        #imgDoc.drawImage(path, 10, 10)
+
+        # イメージを挿入したpdfを作成する
+        # x_posとy_posが画像の挿入位置
         imgDoc.drawImage(path, int(x_pos), int(y_pos))
-        # x, y - start position
-        # in my case -25, -45 needed
         imgDoc.save()
-        # Use PyPDF to merge the image-PDF into the template
+
+        # 画像付きpdfを生成 → overlay.pdf
         pdf.addPage(pypdf.PdfFileReader(
             BytesIO(imgTemp.getvalue())).getPage(0))
-
         pdf.write(open("overlay.pdf", "wb"))
 
+        # 作成したpdfと、画像を貼り付けたいpdfとをマージしていく
         with open("original/"+original_file, "rb") as inFile, open("overlay.pdf", "rb") as overlay:
             original = pypdf.PdfFileReader(inFile)
             print(original.numPages)
@@ -48,20 +45,20 @@ def gen_pdf(original_file_list):
                 print(original_file + ":" + str(i)+"page done!")
                 background = original.getPage(i)
                 foreground = pypdf.PdfFileReader(overlay).getPage(0)
-                # merge the first two pages
                 background.mergePage(foreground)
 
-            # add all pages to a writer
+            # 各ページに書き込み処理を行う
             writer = pypdf.PdfFileWriter()
             for i in range(original.getNumPages()):
                 page = original.getPage(i)
                 writer.addPage(page)
 
-            # write everything in the writer to a file
+            # ファイル化
             with open("modified/"+original_file[:-4]+"_modified.pdf", "wb") as outFile:
                 writer.write(outFile)
 
 
+# Python本より利用 フォルダを渡り歩いてファイル名のリストを得る
 def walk_path(path):
     file_list = []
     for foldername, subfolders, filenames in os.walk(path):
@@ -73,4 +70,4 @@ def walk_path(path):
 
 if __name__ == '__main__':
     gen_pdf(walk_path("./original"))
-    messagebox.showinfo('完了', '出力が完了しました。')
+    print("処理が完了しました")
